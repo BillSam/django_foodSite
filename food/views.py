c24ac8cc1e4414e2be3e1d613d2f47fc4da41b0c
mbugua <email@email.com>
2021-02-21 11:08:41 +0300

    django apps retrieve data

---
+++ food/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Item
+
+
+# Create your views here.
+def index(request):
+    item_list = Item.objects.all()
+    return HttpResponse(item_list)
+
+
+def item(request):
+    return HttpResponse('<h1>This is as item view<h1>')
