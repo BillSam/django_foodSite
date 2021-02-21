dcf401cc3abbffc7dabd8be08bbf7708be1966f7
mbugua <email@email.com>
2021-02-21 13:00:38 +0300

    remove hardcodded url

--- food/urls.py
+++ food/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('item/', views.item, name='item')
+    path('<int:item_id>', views.detail, name='detail'),
+    path('item/', views.items, name='item'),
+
 ]
--- food/views.py
+++ food/views.py
@@ -1,13 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Item
+from django.template import loader
 
 
 # Create your views here.
 def index(request):
     item_list = Item.objects.all()
-    return HttpResponse(item_list)
+    # template = loader.get_template('food/index.html')
+    context = {
+        'item_list': item_list,
+    }
+    return render(request, 'food/index.html', context)
 
 
-def item(request):
+def items(request):
     return HttpResponse('<h1>This is as item view<h1>')
+
+
+def detail(request, item_id):
+    item = Item.objects.get(pk=item_id)
+    context = {
+        'item': item,
+    }
+    return render(request, 'food/detail.html', context)
